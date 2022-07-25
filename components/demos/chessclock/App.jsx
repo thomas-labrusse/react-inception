664c4558685e5d17d6fe004f96940018322c5c44
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-07-25 14:57:51 +0200

    isolating reducer

--- components/demos/chessclock/App.jsx
+++ components/demos/chessclock/App.jsx
@@ -1,73 +1,12 @@
-import React, { useState, useEffect, useCallback, useReducer } from 'react'
+import React, { useEffect, useCallback, useReducer } from 'react'
 
 import styles from './App.module.css'
 
 import Timer from './Timer'
 import FullTag from '../../layout/demos/FullTag'
+import { reducer, initialState } from './reducer'
 
 const App = () => {
-	const reducer = (state, action) => {
-		switch (action.type) {
-			case 'timer_clicked': {
-				// first timer to be clicked starts
-				// no timer can be played if the game is paused
-				if (!state.activePlayer && !state.paused) {
-					// returns next state for React to set
-					return { ...state, activePlayer: action.id, activeTimer: action.id }
-				}
-
-				// guard clause to prevent unactive player to control the timers
-				if (state.activeTimer !== action.id) return { ...state }
-
-				// toggle active player
-				if (action.id === 1) {
-					return { ...state, activePlayer: 2, activeTimer: 2 }
-				} else if (action.id === 2) {
-					return { ...state, activePlayer: 1, activeTimer: 1 }
-				}
-			}
-			case 'pause': {
-				if (!state.activePlayer || state.gameOver) return { ...state }
-				if (state.paused) {
-					return {
-						...state,
-						activeTimer: state.activePlayer,
-						paused: !state.paused,
-					}
-				} else {
-					return {
-						...state,
-						activeTimer: null,
-						paused: !state.paused,
-					}
-				}
-			}
-			case 'reset': {
-				return { ...initialState }
-			}
-			case 'decrease_timer': {
-				return action.id === 1
-					? { ...state, timer1: state.timer1 - 1 }
-					: { ...state, timer2: state.timer2 - 1 }
-			}
-			case 'game_over': {
-				return { ...state, paused: true, gameOver: true, activeTimer: null }
-			}
-			default: {
-				throw Error('Unknown action: ' + action.type)
-			}
-		}
-	}
-
-	const initialState = {
-		activePlayer: null,
-		activeTimer: null,
-		gameOver: false,
-		paused: false,
-		timer1: 10,
-		timer2: 10,
-	}
-
 	const [state, dispatch] = useReducer(reducer, initialState)
 
 	const handleTimerClicked = (id) => {
@@ -134,10 +73,10 @@ const App = () => {
 				/>
 			</div>
 			<div className={styles['control-panel']}>
-				<button onClick={handlePause} className={styles.button}>
+				<button onClick={handlePause} className='btn'>
 					{state.paused ? 'Resume' : 'Pause'}
 				</button>
-				<button onClick={handleReset} className={styles.button}>
+				<button onClick={handleReset} className='btn'>
 					Reset
 				</button>
 			</div>
---
+++ components/demos/chessclock/reducer.js
@@ -0,0 +1,61 @@
+export const reducer = (state, action) => {
+	switch (action.type) {
+		case 'timer_clicked': {
+			// first timer to be clicked starts
+			// no timer can be played if the game is paused
+			if (!state.activePlayer && !state.paused) {
+				// returns next state for React to set
+				return { ...state, activePlayer: action.id, activeTimer: action.id }
+			}
+
+			// guard clause to prevent unactive player to control the timers
+			if (state.activeTimer !== action.id) return { ...state }
+
+			// toggle active player
+			if (action.id === 1) {
+				return { ...state, activePlayer: 2, activeTimer: 2 }
+			} else if (action.id === 2) {
+				return { ...state, activePlayer: 1, activeTimer: 1 }
+			}
+		}
+		case 'pause': {
+			if (!state.activePlayer || state.gameOver) return { ...state }
+			if (state.paused) {
+				return {
+					...state,
+					activeTimer: state.activePlayer,
+					paused: !state.paused,
+				}
+			} else {
+				return {
+					...state,
+					activeTimer: null,
+					paused: !state.paused,
+				}
+			}
+		}
+		case 'reset': {
+			return { ...initialState }
+		}
+		case 'decrease_timer': {
+			return action.id === 1
+				? { ...state, timer1: state.timer1 - 1 }
+				: { ...state, timer2: state.timer2 - 1 }
+		}
+		case 'game_over': {
+			return { ...state, paused: true, gameOver: true, activeTimer: null }
+		}
+		default: {
+			throw Error('Unknown action: ' + action.type)
+		}
+	}
+}
+
+export const initialState = {
+	activePlayer: null,
+	activeTimer: null,
+	gameOver: false,
+	paused: false,
+	timer1: 10,
+	timer2: 10,
+}
